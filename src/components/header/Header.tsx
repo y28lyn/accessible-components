0e5f1e4c4a6056c8ef6d6b17b2fed94d87bae34d
y28lyn <i.beirade@gmail.com>
2024-01-15 16:48:45 +0100

    style: :lipstick: Header title updated

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="relative flex flex-col-reverse py-28 lg:pt-0 lg:flex-col lg:pb-0 overflow-hidden h-screen">
+    <div className="relative flex flex-col-reverse py-36 lg:pt-0 lg:flex-col lg:pb-0 overflow-hidden h-screen">
       <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
         <svg
           className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
