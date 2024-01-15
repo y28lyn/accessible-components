f33dff62dcaf1af279056f01963e8e95b9254869
y28lyn <i.beirade@gmail.com>
2024-01-15 15:05:39 +0100

    style: :art: Header's style improved

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 overflow-hidden h-screen">
+    <div className="relative flex flex-col-reverse py-28 lg:pt-0 lg:flex-col lg:pb-0 overflow-hidden h-screen">
       <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
         <svg
           className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
@@ -11,7 +11,7 @@ export const Header = () => {
           <path d="M50 0H100L50 100H0L50 0Z" />
         </svg>
         <img
-          className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+          className="object-cover w-full h-80 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
           src="https://images.unsplash.com/photo-1526925539332-aa3b66e35444?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
           role="presentation"
--- src/components/items/Title.tsx
+++ src/components/items/Title.tsx
@@ -10,7 +10,7 @@ const Title: React.FC<TitleProps> = (props) => {
       <h1 className="text-4xl font-extrabold p-6 pb-2 text-white">
         {props.title}
       </h1>
-      <div className="w-[98%] h-[2px] bg-white mx-auto"></div>
+      <div className="w-[90%] md:w-[98%] h-[2px] bg-white mx-auto"></div>
     </>
   );
 };
